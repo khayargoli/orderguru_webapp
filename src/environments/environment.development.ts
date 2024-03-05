e253fd6265c001e8770f5c26049d24aee6372ab0
Biswas Khayargoli <developer@skred.me>
2024-03-05 18:05:58 +0545

    new

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+      apiKey: "AIzaSyBJN3-OPmUzlLMvSTh5xt61XhTeZemogEU",
+      authDomain: "orderguru.firebaseapp.com",
+      projectId: "orderguru",
+      storageBucket: "orderguru.appspot.com",
+      messagingSenderId: "254449845828",
+      appId: "1:254449845828:web:1c34773554b4d9a143de11",
+      measurementId: "G-CK8GQLEXVF"
+    }
+  };
+  
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+      apiKey: "AIzaSyBJN3-OPmUzlLMvSTh5xt61XhTeZemogEU",
+      authDomain: "orderguru.firebaseapp.com",
+      projectId: "orderguru",
+      storageBucket: "orderguru.appspot.com",
+      messagingSenderId: "254449845828",
+      appId: "1:254449845828:web:6c383348e8bdd0ef43de11",
+      measurementId: "G-HZGDZX3QEW"
+    };
+  };
+  
